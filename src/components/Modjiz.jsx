e3e18c27d9e2ab9bfc810a64ce5945cef3a50a68
PYXHD <161946414+PYXHD@users.noreply.github.com>
2026-08-10 17:09:15 +0200

    adjust Modjiz component

--- src/components/Modjiz.jsx
+++ src/components/Modjiz.jsx
@@ -1,6 +1,10 @@
 import iconNext from "../assets/icons/stack_next.svg";
 import iconSass from "../assets/icons/stack_sass.svg";
 import iconThree from "../assets/icons/stack_three.svg";
+import iconSupabase from "../assets/icons/stack_supabase.svg";
+import iconTs from "../assets/icons/stack_ts.svg";
+import iconExpo from "../assets/icons/stack_expo.svg";
+
 import blueGithub from "../assets/icons/med_blueGithub.svg";
 
 import modjizPic from "../assets/images/modjiz.webp";
@@ -14,8 +18,10 @@ function Modjiz() {
           <h3 className="project_title">Modjiz</h3>
           <div className="project_icons">
             <img src={iconNext} alt="Next Icon" />
-            <img src={iconSass} alt="Sass Icon" />
+            <img src={iconTs} alt="Typescript Icon" />
+            <img src={iconSupabase} alt="Supabase Icon" />
             <img src={iconThree} alt="Three Icon" />
+            <img src={iconExpo} alt="Expo Icon" />
           </div>
         </div>
         <img className="project_img" src={modjizPic} alt="Modjiz" />
@@ -24,12 +30,11 @@ function Modjiz() {
         <p className="project_text">
           Application de suivi de l’humeur avec saisie quotidienne, historique
           et analyse statistique. Visualisation des émotions via un personnage
-          3D interactif (Three.js, Blender), renforçant l’engagement
-          utilisateur.
+          3D interactif.
         </p>
         <p className="project_text">
-          Développée avec Next.js, React et TypeScript, avec mise en place de
-          tests (Vitest).
+          Développée pour le web et le mobile, avec une architecture de données
+          centralisée.
         </p>
       </div>
       <div className="project_btnInner">
